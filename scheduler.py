afe35d62bee8b6d55425ac7c08fb3a966262ae4d
Randy Mejia Arias <randymejiaarias@gmail.com>
2025-01-07 01:19:53 -0500

    Feature: Add schedule for update prices and calculate errors

--- scheduler.py
+++ scheduler.py
@@ -1,16 +1,19 @@
 import schedule
 import time
-from database import input_collection
-from scraper import scrape_and_store
 from dotenv import load_dotenv
 import os
+from database import input_collection
+from scraper import scrape_and_store
+from predicter import daily_prediction, update_real_prices, calculate_updated_errors
 
 # Cargar variables de entorno
 load_dotenv()
 
 # Horarios desde variables de entorno
 EXECUTION_TIME_1 = os.getenv("EXECUTION_TIME_1", "06:00")
-EXECUTION_TIME_2 = os.getenv("EXECUTION_TIME_2", "18:00")
+EXECUTION_TIME_2 = os.getenv("EXECUTION_TIME_2", "06:20")
+EXECUTION_TIME_3 = os.getenv("EXECUTION_TIME_3", "06:40")
+EXECUTION_TIME_4 = os.getenv("EXECUTION_TIME_4", "07:00")
 
 def process_documents():
     print("Iniciando proceso de scraping...")
@@ -34,7 +37,9 @@ def process_documents():
 
 # Programar las ejecuciones
 schedule.every().day.at(EXECUTION_TIME_1).do(process_documents)
-schedule.every().day.at(EXECUTION_TIME_2).do(process_documents)
+schedule.every().day.at(EXECUTION_TIME_2).do(daily_prediction)
+schedule.every().day.at(EXECUTION_TIME_3).do(update_real_prices)
+schedule.every().day.at(EXECUTION_TIME_4).do(calculate_updated_errors)
 
 def start_scheduler():
     print("Iniciando programador...")

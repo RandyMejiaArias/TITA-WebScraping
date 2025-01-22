7755acbefcaa3566ec8b3870f4b7b215c6b79826
Randy Mejia <randymejiaarias@gmail.com>
2025-01-22 16:50:22 +0000

    Feature: Add cron jobs to update datamarts on PowerBI and fix null prices

--- scheduler.py
+++ scheduler.py
@@ -3,24 +3,29 @@ import time
 from dotenv import load_dotenv
 import os
 from database import input_collection
+from checker import calculate_updated_errors, fix_null_prices,update_real_prices
+from predicter import daily_prediction
 from scraper import scrape_and_store
-from predicter import daily_prediction, update_real_prices, calculate_updated_errors
+from updater import etl_update
 
 # Cargar variables de entorno
 load_dotenv()
 
 # Horarios desde variables de entorno
-EXECUTION_TIME_1 = os.getenv("EXECUTION_TIME_1", "05:00")
-EXECUTION_TIME_2 = os.getenv("EXECUTION_TIME_2", "06:00")
-EXECUTION_TIME_3 = os.getenv("EXECUTION_TIME_3", "06:05")
-EXECUTION_TIME_4 = os.getenv("EXECUTION_TIME_4", "06:07")
-EXECUTION_TIME_5 = os.getenv("EXECUTION_TIME_5", "06:09")
+EXECUTION_TIME_1 = os.getenv("EXECUTION_TIME_1", "04:00")
+EXECUTION_TIME_2 = os.getenv("EXECUTION_TIME_2", "04:40")
+EXECUTION_TIME_3 = os.getenv("EXECUTION_TIME_3", "05:20")
+EXECUTION_TIME_4 = os.getenv("EXECUTION_TIME_4", "06:00")
+EXECUTION_TIME_5 = os.getenv("EXECUTION_TIME_5", "06:05")
+EXECUTION_TIME_6 = os.getenv("EXECUTION_TIME_6", "06:07")
+EXECUTION_TIME_7 = os.getenv("EXECUTION_TIME_7", "06:09")
+EXECUTION_TIME_8 = os.getenv("EXECUTION_TIME_8", "06:15")
 
 def process_documents():
     print("Iniciando proceso de scraping...")
     try:
         # Obtener documentos desde la base de datos
-        documentos = input_collection.find()  # Ajusta el query si necesitas filtrar datos específicos
+        documentos = input_collection.find()
 
         for doc in documentos:
             url = doc.get("url")
@@ -39,9 +44,12 @@ def process_documents():
 # Programar las ejecuciones
 schedule.every().day.at(EXECUTION_TIME_1).do(process_documents)
 schedule.every().day.at(EXECUTION_TIME_2).do(process_documents)
-schedule.every().day.at(EXECUTION_TIME_3).do(daily_prediction)
-schedule.every().day.at(EXECUTION_TIME_4).do(update_real_prices)
-schedule.every().day.at(EXECUTION_TIME_5).do(calculate_updated_errors)
+schedule.every().day.at(EXECUTION_TIME_3).do(process_documents)
+schedule.every().day.at(EXECUTION_TIME_4).do(daily_prediction)
+schedule.every().day.at(EXECUTION_TIME_5).do(update_real_prices)
+schedule.every().day.at(EXECUTION_TIME_6).do(fix_null_prices)
+schedule.every().day.at(EXECUTION_TIME_7).do(calculate_updated_errors)
+schedule.every().day.at(EXECUTION_TIME_8).do(etl_update)
 
 def start_scheduler():
     print("Iniciando programador...")
